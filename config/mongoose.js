4854304c3d9732788a36eaba3aa8a2f76eccd37a
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-09-23 13:43:31 +0530

    Mongo Updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 //Set up default mongoose connection
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/quotation")
+  .connect(
+    "mongodb+srv://malikabusufyan:f3TZNp8bmtg3qeXT@cluster0.wu21t2f.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Successfully Connected to Database");
   })
